85d908e0befc5def41b9219b04d366ef2e5a7332
thatipallykoushik <thatipallykoushik@gmail.com>
2021-05-19 19:04:00 +0530

    f1

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const { response } = require('express')
 const express =require('express')
 const request =require('request')
 const app=express()
+const dotenv=require('dotenv')
+dotenv.config()
 //middlewares
 app.set("view engine","ejs")
-
+app.use('/public',express.static('public'))
 //routing
 app.get("/", (req, res)=>{
     
@@ -26,7 +28,7 @@ app.get("/aboutme", (req, res)=>{
     res.render("Aboutme")
 })
 app.get('/result',(req,res)=>{
-    const url=`http://www.omdbapi.com/?apikey=ea18075e&s=${req.query.movieName}`
+    const url=`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${req.query.movieName}`
     request(url,function(error,reponse,body){
         if(!error && response.statusCode===200){
             const data=JSON.parse(body)
@@ -42,7 +44,7 @@ app.get('/result',(req,res)=>{
 app.get("/result/:id", (req, res)=>{
     //console.log(req.query.movieName)
     console.log(process.env.API_KEY)
-    const url= `http://www.omdbapi.com/?apikey=ea18075e&i=${req.params.id}`
+    const url= `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${req.params.id}`
     request(url, function (error, response, body){
         if(!error && response.statusCode==200){
             const data= JSON.parse(body)
@@ -58,7 +60,7 @@ app.get("/result/:id", (req, res)=>{
 app.get('/home',(req,res)=>{
 
             
-    const url="http://www.omdbapi.com/?apikey=ea18075e&s=avengers"
+    const url=`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=avengers`
     request(url,function(error,reponse,body){
         if(!error && response.statusCode===200){
             const data=JSON.parse(body)
