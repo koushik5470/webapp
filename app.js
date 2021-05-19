bede7d45fea5bd88dd7027d2cb2611b91ce12bbb
thatipallykoushik <thatipallykoushik@gmail.com>
2021-05-19 17:54:03 +0530

    4

--- app.js
+++ app.js
@@ -1,22 +1,77 @@
+const { response } = require('express')
 const express =require('express')
+const request =require('request')
 const app=express()
 //middlewares
 app.set("view engine","ejs")
 
 //routing
+app.get("/", (req, res)=>{
+    
+           
+    const url="http://www.omdbapi.com/?apikey=ea18075e&s=avengers"
+    request(url,function(error,reponse,body){
+        if(!error && response.statusCode===200){
+            const data=JSON.parse(body)
+            //res.send(data)
+            res.render('home',{moviedump1:data})
+        }else{
+            res.send('somthing went wrong')
+        }
+    })
+       
+    
+})
+app.get("/aboutme", (req, res)=>{
+    res.render("Aboutme")
+})
 app.get('/result',(req,res)=>{
+    const url=`http://www.omdbapi.com/?apikey=ea18075e&s=${req.query.movieName}`
+    request(url,function(error,reponse,body){
+        if(!error && response.statusCode===200){
+            const data=JSON.parse(body)
+            //res.send(data)
+            res.render('results',{moviedump:data})
+        }else{
+            res.send('somthing went wrong')
+        }
+    })
     //res.send('home page ')
-    res.render("results")//displays html file
+   // res.render("results")//displays html file
 })
-app.get('/',(req,res)=>{
-    //res.send('home page ')
-    res.render("home")//displays html file
-})
-app.get('/hi/:id',(req,res)=>{
-    console.log(req.params)
-    res.send(`home page hi ${req.params.id}`)
+app.get("/result/:id", (req, res)=>{
+    //console.log(req.query.movieName)
+    console.log(process.env.API_KEY)
+    const url= `http://www.omdbapi.com/?apikey=ea18075e&i=${req.params.id}`
+    request(url, function (error, response, body){
+        if(!error && response.statusCode==200){
+            const data= JSON.parse(body)
+            //console.log(data)
+            //res.send(data)
+            res.render("details", {movieData: data})
+        }else{
+            res.send("Uh oh error")
+        }
+    })
 })
 
+app.get('/home',(req,res)=>{
+
+            
+    const url="http://www.omdbapi.com/?apikey=ea18075e&s=avengers"
+    request(url,function(error,reponse,body){
+        if(!error && response.statusCode===200){
+            const data=JSON.parse(body)
+            //res.send(data)
+            res.render('home',{moviedump1:data})
+        }else{
+            res.send('somthing went wrong')
+        }
+    })
+})
+app.get("*", (req, res)=>{
+    res.send("Go back! Illegal response")
+})
 app.listen(3000,()=>{
     console.log("server has strated")
 })
